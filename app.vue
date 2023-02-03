ad179bd4990e22beeac9d156a65037a50b2a3f2b
Yago <yago.mbarreto@hotmail.com>
2023-02-03 16:56:17 -0300

    Finalizando tratamento dos dados/paginação e recebendo dados diretamente do back-end

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="users__wrapper">
+  <div class="users__wrapper" v-if="isNextPageEmpty">
     <div class="users__card-wrapper" v-for="user in userList">
       <img class="users__card-img" :src="user.avatar"/>
       <div class="users__card-info-wrapper">
@@ -18,11 +18,15 @@
       </div>
     </div>
   </div>
+  <div class="users__wrapper-empty" v-else>
+    <div class="users__empty">A lista não possui segunda página.</div>
+  </div>
   <div class="users__pagination-wrapper">
     <div class="users__pagination-title">Pages</div>
     <div class="users__pagination">
-      <div class="users__pagination-cards" :class="{'users__pagination-card-current' : currentPage = pages}" v-for="pages in totalPages">
-        {{ pages }}
+      <div class="users__pagination-cards" :class="{'users__pagination-card-current' : currentPageTemp == pages}" v-for="pages in totalPages">
+        <div @click="goToPage(pages)">{{ pages }}</div>
+        <div @click="goToPage(pages)">{{ currentPage }}</div>
       </div>
     </div>
   </div>
@@ -37,19 +41,37 @@ export default {
       userList: null,
       totalPages: 0,
       currentPage: 1,
+      currentPageTemp: 1,
+      isNextPageEmpty: false,
     }
   },
   methods: {
     getUsers() {
       var vm = this;
       axios
-        .get("https://reqres.in/api/users")
+        .get("http://127.0.0.1:8080/users")
         .then(response => {
-          vm.userList = response.data.data;
-          vm.totalPages = response.data.total_pages;
-          console.log(response.data);
+          var tempReponse = JSON.parse(response.data);
+          vm.userList = tempReponse.data;
+          vm.totalPages = tempReponse.total_pages;
+          console.log(tempReponse);
         });
     },
+    goToPage(page) {
+      var vm = this;
+      vm.currentPage = page;
+      vm.currentPageTemp = page;
+    }
+  },
+  computed: {
+    currentPage() {
+      var vm = this;
+      if(vm.currentPage != 2 && vm.userList.length <= 6) {
+        vm.isNextPageEmpty = true;
+      } else {
+        vm.isNextPageEmpty = false;
+      }
+    }
   },
   mounted() {
       var vm = this;
